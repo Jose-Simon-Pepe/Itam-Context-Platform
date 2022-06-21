3033d1ded3be7fdba95e41c3b13a98f8012db7e3
jose pepe <alessandrovmkt@gmail.com>
2022-06-21 19:54:19 -0300

    agregando a github

---
+++ src/main/java/com/itamcontext/itampage/ItampageApplication.java
@@ -0,0 +1,13 @@
+package com.itamcontext.itampage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItampageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItampageApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/itamcontext/itampage/ItampageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itamcontext.itampage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItampageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
